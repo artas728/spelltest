d1609be96f99c3b5c260e97ac5500aa713fd9385
artas728 <andrew.volotscov@gmail.com>
2023-11-07 11:28:00 -0800

    update version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='spelltest',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=required,
     include_package_data=True,
